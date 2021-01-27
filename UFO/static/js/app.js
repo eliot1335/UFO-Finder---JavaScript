bd99846c573d8de7b83f0efc8d68c057485fb77b
Eliot Chern <eliotchern@gmail.com>
2021-01-26 22:23:41 -0800

    clean up a bit

--- UFO/static/js/app.js
+++ UFO/static/js/app.js
@@ -6,48 +6,16 @@ var tbody = d3.select("tbody");
 
 // Try to build the table
 
-// // Step 1: Loop Through `data` and console.log each object
-// data.forEach(function(ufoData) {
-//     console.log(ufoData);
-// })
-
-// // Step 2:  Use d3 to append one table row `tr` for each object
-// data.forEach(function(ufoData) {
-//   console.log(ufoData);
-//   var row = tbody.append("tr");
-// });
-
+// Step 1: Loop Through `data` and console.log each object
+// Step 2:  Use d3 to append one table row `tr` for each object
 // Step 3:  Use `Object.entries` to console.log each value
-// data.forEach(function(ufoData) {
-//   console.log(ufoData);
-//   var row = tbody.append("tr");
-
-//   Object.entries(ufoData).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
-
 // Step 4: Use d3 to append 1 cell per sighting report value (date/time, city, state, country, shape, and comment)
-// data.forEach(function(ufoData) {
-//   console.log(ufoData);
-//   var row = tbody.append("tr");
-
-//   Object.entries(ufoData).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     var cell = row.append("td");
-//   });
-// });
-
-// Step 5: Use d3 to update each cell's text with
-// sighting report values (date/time, city, state, country, shape, and comment)
-data.forEach(function(ufoData) {
-  console.log(ufoData);
-  var row = tbody.append("tr");
-  Object.entries(ufoData).forEach(function([key, value]) {
-    console.log(key, value);
-    // Append a cell to the row for each value
-    var cell = row.append("td");
-    cell.text(value);
+// Step 5: Use d3 to update each cell's text with sighting report values
+// Simplefied using Arrow Functions
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
-});
