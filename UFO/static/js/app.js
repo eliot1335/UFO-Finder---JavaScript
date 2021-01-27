3230c2813d7410fe58c78b77e32be8bf810f06ac
Eliot Chern <eliotchern@gmail.com>
2021-01-26 22:20:18 -0800

    able to update the table to page

--- UFO/static/js/app.js
+++ UFO/static/js/app.js
@@ -26,3 +26,28 @@ var tbody = d3.select("tbody");
 //     console.log(key, value);
 //   });
 // });
+
+// Step 4: Use d3 to append 1 cell per sighting report value (date/time, city, state, country, shape, and comment)
+// data.forEach(function(ufoData) {
+//   console.log(ufoData);
+//   var row = tbody.append("tr");
+
+//   Object.entries(ufoData).forEach(function([key, value]) {
+//     console.log(key, value);
+//     // Append a cell to the row for each value
+//     var cell = row.append("td");
+//   });
+// });
+
+// Step 5: Use d3 to update each cell's text with
+// sighting report values (date/time, city, state, country, shape, and comment)
+data.forEach(function(ufoData) {
+  console.log(ufoData);
+  var row = tbody.append("tr");
+  Object.entries(ufoData).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
