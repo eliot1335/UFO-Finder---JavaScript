56086cb1bcec617c668deed5e9b05b17cd87ae8b
Eliot Chern <eliotchern@gmail.com>
2021-01-26 22:15:37 -0800

    following class activaty instruction

--- UFO/static/js/app.js
+++ UFO/static/js/app.js
@@ -5,6 +5,24 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Try to build the table
-data.forEach(function(ufoData) {
-    console.log(ufoData);
-})
+
+// // Step 1: Loop Through `data` and console.log each object
+// data.forEach(function(ufoData) {
+//     console.log(ufoData);
+// })
+
+// // Step 2:  Use d3 to append one table row `tr` for each object
+// data.forEach(function(ufoData) {
+//   console.log(ufoData);
+//   var row = tbody.append("tr");
+// });
+
+// Step 3:  Use `Object.entries` to console.log each value
+// data.forEach(function(ufoData) {
+//   console.log(ufoData);
+//   var row = tbody.append("tr");
+
+//   Object.entries(ufoData).forEach(function([key, value]) {
+//     console.log(key, value);
+//   });
+// });
